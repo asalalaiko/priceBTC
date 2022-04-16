05fd2e83ff142125d390996d472849e4167cea19
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-04-17 00:43:23 +0300

    start new microservice

---
+++ priceBtcStatistics/src/main/java/by/asalalaiko/priceBtcStatistics/PriceBtcStatisticsApplication.java
@@ -0,0 +1,13 @@
+package by.asalalaiko.priceBtcStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceBtcStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceBtcStatisticsApplication.class, args);
+	}
+
+}
---
+++ priceBtcStatistics/src/test/java/by/asalalaiko/priceBtcStatistics/PriceBtcStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.asalalaiko.priceBtcStatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceBtcStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
